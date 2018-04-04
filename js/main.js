6c7e5d06999758363c6a2ae7d8b77afb0796891e
David Praca <dsestuller@gmail.com>
2018-04-04 22:06:12 +0200

    pridane info o prvom obdobi, ymenena animacia tlacitiek

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/* animacia tlacitiek
 $("#kronika").on( "click",function() {
 	if($("#pozadie").hasClass("kronika")==false){
 		$("#pozadie").animate({
@@ -44,5 +45,54 @@ $("#main_text").on( "click",function() {
 		$("#main_text").animate({
 			color:"white"
 		},500,"swing");
+	}
+});
+
+window.scrollBy({ //smooth scroll
+  top: 100, 
+  left: 0, 
+  behavior: 'smooth' 
+});*/
+$('a[href*="#"]')
+	// Remove links that don't actually link to anything
+	.not('[href="#"]')
+	.not('[href="#0"]')
+	.click(function(event) {
+    // On-page links
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname){
+		// Figure out element to scroll to
+		var target = $(this.hash);
+		target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+		// Does a scroll target exist?
+		if (target.length) {
+			// Only prevent default if animation is actually gonna happen
+			event.preventDefault();
+			$('html, body').animate({
+				scrollTop: target.offset().top
+			}, 1000, function() {
+				// Callback after animation
+				// Must change focus!
+				var $target = $(target);
+				$target.focus();
+				if ($target.is(":focus")) { // Checking if the target was focused
+					return false;
+				} else {
+					$target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+					$target.focus(); // Set focus again
+				};
+			});
+		}
+    }
+});
+$("#kronika").on( "click",function() {
+	if($(this).hasClass("buttonActive")==false){
+		$(this).addClass("buttonActive");
+		$("#main_text").removeClass("buttonActive");
+	}
+});
+$("#main_text").on( "click",function() {
+	if($(this).hasClass("buttonActive")==false){
+		$(this).addClass("buttonActive");
+		$("#kronika").removeClass("buttonActive");
 	}
 });
