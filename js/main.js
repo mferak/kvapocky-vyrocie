5986558dde7a2c5c3ba8cc0ea85dd50ecb1fa8a4
David Dom <dsestuller@gmail.com>
2018-04-04 14:32:02 +0200

    animacia tlacitiek (bude zmenena)

--- js/main.js
+++ js/main.js
@@ -1,2 +1,48 @@
-function main(){
-}
+$("#kronika").on( "click",function() {
+	if($("#pozadie").hasClass("kronika")==false){
+		$("#pozadie").animate({
+			left:"218px"
+		},{
+		  queue: false,
+		  duration: 500,
+		  easing: "swing"
+		}).animate({
+			width:"436px"
+			},400,"swing", function() {
+				$("#pozadie").animate({
+					width:"218px"
+				},100,"swing");
+		});
+		$("#pozadie").addClass("kronika");
+		$("#kronika").animate({
+			color:"white"
+		},500,"swing");
+		$("#main_text").animate({
+			color:"black"
+		},500,"swing");
+	}
+});
+$("#main_text").on( "click",function() {
+	if($("#pozadie").hasClass("kronika")){
+		$("#pozadie").animate({
+			left:"0px"
+		},{
+		  queue: false,
+		  duration: 500,
+		  easing: "swing"
+		}).animate({
+			width:"436px"
+			},100,"swing", function() {
+				$("#pozadie").animate({
+					width:"218px"
+				},400,"swing");
+		});
+		$("#pozadie").removeClass("kronika");
+		$("#kronika").animate({
+			color:"black"
+		},500,"swing");
+		$("#main_text").animate({
+			color:"white"
+		},500,"swing");
+	}
+});
